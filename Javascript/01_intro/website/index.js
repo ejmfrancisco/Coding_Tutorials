9a4f3a505fc2521e149260ee34ae00e98fe35b0c
ejmfrancisco <ejcaliwag.emfc@outlook.com>
2024-06-26 18:31:37 +0900

    Add JS approach.

--- Javascript/01_intro/website/index.js
+++ Javascript/01_intro/website/index.js
@@ -1,4 +1,14 @@
+document.getElementById('myH1').textContent= 'Hello from JS!';
+document.getElementById('myP').textContent= 'I like coding!';
+
 console.log(`Hello!`);
 console.log(`Another message.`);
 
 window.alert(`This is an alert!!!`);
+
+
+// Notes for myself
+
+/*
+    This is a multi-line comment
+*/
