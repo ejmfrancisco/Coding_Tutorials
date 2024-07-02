c0f9d58ccb9fac2d84dae97e54bc40e6a3ed2ae4
ejmfrancisco <ejcaliwag.emfc@outlook.com>
2024-07-02 18:29:57 +0900

    Add variables.

---
+++ Javascript/02_variables/index.js
@@ -0,0 +1,13 @@
+// variable = A container that stores a value.
+//                   Behaves as if it were the value it contains.
+
+let fullName = "ejmfrancisco";
+let age = 25;
+let isHandsome = true;
+
+document.getElementById("p1").textContent = 'Your id is ${fullName}';
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isHandsome}`;
+
+console.log(`Hello!`);
+console.log(fullName);
