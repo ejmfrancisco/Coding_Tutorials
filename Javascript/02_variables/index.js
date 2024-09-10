d0875b7ec8444299b149522348944537445c44c3
Ej Caliwag <85476594+ejmfrancisco@users.noreply.github.com>
2024-09-10 18:29:21 +0900

    Update index.js

--- Javascript/02_variables/index.js
+++ Javascript/02_variables/index.js
@@ -4,10 +4,12 @@
 let fullName = "EJ 이제이";
 let age = 25;
 let isHandsome = true;
+let isRich = true;
 
 document.getElementById("p1").textContent = 'Your id is ${fullName}';
 document.getElementById("p2").textContent = `You are ${age} years old`;
 document.getElementById("p3").textContent = `Enrolled: ${isHandsome}`;
+document.getElementById("p4").textContent = `Enrolled: ${isRich}`;
 
 console.log(`Hello!`);
 console.log(fullName);
